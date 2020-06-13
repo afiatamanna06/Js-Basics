43c25a1dd8010436f17b1315b9ebbcf4b35c004f
afiatamanna06 <afiatamanna06@gmail.com>
2020-06-13 19:25:19 +0600

    Variables.js updated

--- variables.js
+++ variables.js
@@ -1,6 +1,11 @@
 let firstName = "Hanajima"
 let lastName = "Saki"
 const number = 1
+let isApproved = true
+let selectedColor = null
+let selectedType = undefined
 console.log(firstName)
 console.log(lastName)
 console.log(number)
+console.log(selectedColor)
+console.log(selectedType)
