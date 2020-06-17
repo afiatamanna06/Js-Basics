891173e6427da5f45497b87fa3b8ea1f9bfd4277
afiatamanna06 <afiatamanna06@gmail.com>
2020-06-17 21:18:54 +0600

    functions.js updated

--- functions.js
+++ functions.js
@@ -2,3 +2,8 @@ function greet(firstName, lastName) {
     console.log('Hello ' + firstName + ' ' + lastName);
 }
 greet('Sarah', 'Sabrin');
+
+function square(number) {
+    return number * number;
+}
+console.log(square(2))
