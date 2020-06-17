41141a847117987fac9511f1c6a216b0fa5eaa2d
afiatamanna06 <afiatamanna06@gmail.com>
2020-06-17 20:49:18 +0600

    arrays.js updated

--- arrays.js
+++ arrays.js
@@ -1,2 +1,9 @@
 let selectedColors = ['red', 'blue'];
-console.log(selectedColors)
+console.log(selectedColors);
+console.log(selectedColors[0]);
+selectedColors[2] = 'green';
+console.log(selectedColors);
+selectedColors[3] = 1;
+console.log(selectedColors);
+console.log(selectedColors.length);
+console.log(typeof selectedColors);
