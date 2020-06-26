76c7e1bf3aad54a4d9bf050ad6d3ea079945abd8
afiatamanna06 <afiatamanna06@gmail.com>
2020-06-26 18:48:39 +0600

    letandvar.js updated

---
+++ letandvar.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    if (true) {
+        var color = 'red';
+        console.log(color + ' printed');
+    }
+
+    console.log(color + ' printed');
+}
+
+myfunction(); //let and const block scoped and var function scoped
