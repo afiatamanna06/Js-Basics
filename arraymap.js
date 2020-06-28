ad2136c663c963021dfcf23f20c6e0cbc916b9d8
afiatamanna06 <afiatamanna06@gmail.com>
2020-06-28 20:39:42 +0600

    arraymap.js updated

--- arraymap.js
+++ arraymap.js
@@ -1,4 +1,5 @@
 let numbers = [1, -1, -100, 2];
 let positiveNumbers = numbers.filter(n => n >= 0);
 let listedNumbers = positiveNumbers.map(n => '<li>' + n + '</li>');
-console.log(listedNumbers);
+let html = listedNumbers.join('');
+console.log(html);
